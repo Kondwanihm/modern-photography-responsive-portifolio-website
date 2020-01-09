85b59bdaaef9e8b60b5a025a3adfb7bd05fc19c5
darkdante <darkdante@kali.httpsafrisounds.com>
2020-01-09 02:20:40 +0200

    added parallax cover and styling

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
   var $header = $("header");
   var $sticky = $header.before($header.clone().addClass("sticky"));
 
-  $(window).on("scroll", function(){
+  $(window).on("scroll", function() {
     var scrollFromTop = $(window).scrollTop();
-    $("body").toggleClass("scroll", (scrollFromTop > 350));
+    $("body").toggleClass("scroll", scrollFromTop > 350);
   });
-  
+
 });
+
+
